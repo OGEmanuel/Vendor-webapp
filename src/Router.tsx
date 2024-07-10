2de4bf8b79eae91f126de1e037cf62ff070b73f5
Joshua <joshuanwafor01@gmail.com>
2024-07-10 16:43:03 +0100

    installed husky

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { HomePage } from './pages/Dashboard';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-]);
 
 export function Router() {
-  return <RouterProvider router={router} />;
+  return <div>
+    
+  </div>;
 }
