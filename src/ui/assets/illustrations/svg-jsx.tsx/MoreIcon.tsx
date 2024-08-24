db4e12f294f7257760d5978d3f5a2b40638897bc
Emmanuel Ogunmola <1blacksanta416@gmail.com>
2024-08-25 00:56:07 +0100

    add:Inventory desktop UI complete

---
+++ src/ui/assets/illustrations/svg-jsx.tsx/MoreIcon.tsx
@@ -0,0 +1,29 @@
+const MoreIcon = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M11.9922 12H12.0012"
+        stroke="black"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.9844 18H11.9934"
+        stroke="black"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 6H12.009"
+        stroke="black"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default MoreIcon;
